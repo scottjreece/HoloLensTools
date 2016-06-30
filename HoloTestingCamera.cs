0085dfe09d998731177211acd774e88f89420381
scottjreece <scott.reece@gmail.com>
2016-06-29 16:12:21 -0800

    Minor cleanup

--- HoloTestingCamera.cs
+++ HoloTestingCamera.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
 
+/// <summary>
+/// To use this script, attach it to the main camera in your Unity scene. This will allow you to look around with the
+/// mouse, and to move around with the keyboard (WASD by default) while testing your HoloLens app in Unity.
+/// </summary>
 public class HoloTestingCamera : MonoBehaviour
 {
     public float SensitivityX = 8F;
     public float SensitivityY = 8F;
-    public float MinimumX = -360F;
-    public float MaximumX = 360F;
+    public float MoveSpeed = 4;
     public float MinimumY = -89.99F;
     public float MaximumY = 89.99F;
     public bool AddReticle = true;
     public Material ReticleMaterial;
-    public float MoveSpeed = 4;
     public bool HideMouseCursor = true;
     public bool ClampForwardMotionToXZPlane = true;
 
@@ -21,6 +22,7 @@ public class HoloTestingCamera : MonoBehaviour
     private float _rotationY;
     private Quaternion _originalRotation;
 
+    //Replace this Action with something else if you don't want to call an object's OnSelect method when it's clicked on
     public static Action<GameObject> ObjectClickedHandler = x => x.SendMessageUpwards("OnSelect");
 
 #if !UNITY_EDITOR
@@ -42,8 +44,8 @@ public class HoloTestingCamera : MonoBehaviour
 
     void Update()
     {
-        DoMove();
-        DoLook();
+        HandleMovement();
+        HandleLookDirection();
 
         if (Input.GetMouseButtonDown(0))
             HandleMouseClick();
@@ -55,14 +57,14 @@ public class HoloTestingCamera : MonoBehaviour
         var gazeDirection = Camera.main.transform.forward;
 
         RaycastHit hitInfo;
-        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
-        {
-            var targetedObject = hitInfo.collider.gameObject;
-            ObjectClickedHandler(targetedObject);
-        }
+        if (!Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+            return;
+
+        var targetedObject = hitInfo.collider.gameObject;
+        ObjectClickedHandler(targetedObject);
     }
 
-    private void DoMove()
+    private void HandleMovement()
     {
         var forwardDistance = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
         var horizontalDistance = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
@@ -84,13 +86,10 @@ public class HoloTestingCamera : MonoBehaviour
         gameObject.transform.Translate(Vector3.up * verticalDistance, Space.World);
     }
 
-    private void DoLook()
+    private void HandleLookDirection()
     {
-            // Read the mouse input axis
             _rotationX += Input.GetAxis("Mouse X") * SensitivityX;
             _rotationY += Input.GetAxis("Mouse Y") * SensitivityY;
-            _rotationX = ClampAngle(_rotationX, MinimumX, MaximumX);
-            _rotationY = ClampAngle(_rotationY, MinimumY, MaximumY);
             var xQuaternion = Quaternion.AngleAxis(_rotationX, Vector3.up);
             var yQuaternion = Quaternion.AngleAxis(_rotationY, -Vector3.right);
             transform.localRotation = _originalRotation * xQuaternion * yQuaternion;
@@ -98,13 +97,13 @@ public class HoloTestingCamera : MonoBehaviour
 
     private void CreateReticle()
     {
-        AddCube(-.01f, 0, .01f, .005f);
-        AddCube(.01f, 0, .01f, .005f);
-        AddCube(0, .01f, .005f, .01f);
-        AddCube(0, -.01f, .005f, .01f);
+        AddReticleCube(-.01f, 0, .01f, .005f);
+        AddReticleCube(.01f, 0, .01f, .005f);
+        AddReticleCube(0, .01f, .005f, .01f);
+        AddReticleCube(0, -.01f, .005f, .01f);
     }
 
-    private void AddCube(float x, float y, float width, float height)
+    private void AddReticleCube(float x, float y, float width, float height)
     {
         var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         cube.transform.parent = gameObject.transform;
@@ -112,15 +111,6 @@ public class HoloTestingCamera : MonoBehaviour
         cube.transform.localScale = new Vector3(width, height, .01f);
         if (ReticleMaterial != null)
             cube.GetComponent<MeshRenderer>().material = ReticleMaterial;
-    }
-
-    public static float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360F)
-            angle += 360F;
-        if (angle > 360F)
-            angle -= 360F;
-        return Mathf.Clamp(angle, min, max);
     }
 #endif
 }
